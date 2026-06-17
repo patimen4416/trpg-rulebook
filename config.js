4a33673bfadcde088917a36615aff96bf0497fa8
patimen4416 <patimen4416@gmail.com>
2026-06-17 12:14:00 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
+//test
   // Supabase設定（publishable key = 公開可能）
 const SUPABASE_URL = 'https://ukmrcljpqcmxtmiupcpy.supabase.co';
 const SUPABASE_KEY = 'sb_publishable_mW7oVX99Wmn-42J2I-wBHw_YovWTW7G';
